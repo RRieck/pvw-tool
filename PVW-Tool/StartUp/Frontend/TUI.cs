e5f73a8041d5e407f2b44d06baf729036ee31edc
RRieck <rieck@landaumedia.de>
2018-09-17 10:04:05 +0200

    Changed Structure

---
+++ PVW-Tool/StartUp/Frontend/TUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartUp.Frontend
+{
+    class TUI
+    {
+        /// <summary>
+        /// @J Lo => Kannst alles aus Initialize weghauen wenn du magst. Wollte erstmal was zum Testen für für die Struktur haben ; )
+        /// </summary>
+        public TUI()
+        {
+            Initialize();
+            Console.ReadLine();
+        }
+
+        // ReSharper disable once MemberCanBeMadeStatic.Local
+        void Initialize()
+        {
+            while (true)
+            {
+                        Console.Clear();
+                        Console.WriteLine("Starting TUI.");
+                        System.Threading.Thread.Sleep(500);
+
+                        Console.Clear();
+                        Console.WriteLine("Starting TUI. .");
+                        System.Threading.Thread.Sleep(500);
+
+                        Console.Clear();
+                        Console.WriteLine("Starting TUI. . .");
+                        System.Threading.Thread.Sleep(500);
+                }
+        }
+    }
+}
---
+++ PVW-Tool/StartUp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using StartUp.Frontend;
+
+namespace StartUp
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //Open TUI here 
+            var tui = new TUI();
+
+            //Open GUI Here
+            new Application().Run(new GUI());
+        }
+    }
+}
